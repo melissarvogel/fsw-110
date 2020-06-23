f52bc1cedb1f97958053b0f38d41cc3780854d5c
melissarvogel <63073544+melissarvogel@users.noreply.github.com>
2020-06-23 11:27:34 -0400

    Add files via upload

---
+++ week3/forloop_assignment.js
@@ -0,0 +1,14 @@
+
+
+var newText= document.getElementById("theH1Tag")
+newText.textContent = "Hello World"
+
+for(i = 0; i < 10; i++){
+console.log("theH1Tag")
+}
+
+const names = [ "steve", "larry", "joe", "shirley", "steph", "nate", "rick", "emily" ]
+
+for (i = 0; i < names.length; i++){
+    console.log(names.length)
+}
