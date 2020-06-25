9b60d8bdb9d925bec6812538e62c28bb32df93ad
melissarvogel <63073544+melissarvogel@users.noreply.github.com>
2020-06-25 11:23:20 -0400

    Add files via upload

---
+++ week3/changing_styles.js
@@ -0,0 +1,5 @@
+var firstStyle = document.getElementById("theH5")
+firstStyle.style.fontSize = "20px";
+firstStyle.style.fontWeight = "lighter";
+firstStyle.style.fontFamily = 'sans sarif';
+firstStyle.style.color = "cornflowerblue"
