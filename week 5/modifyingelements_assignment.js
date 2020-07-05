262ab741ab2251dfcc426012f1403a453253eb34
melissarvogel <63073544+melissarvogel@users.noreply.github.com>
2020-07-05 14:46:00 -0400

    Add files via upload

---
+++ week 5/modifyingelements_assignment.js
@@ -0,0 +1,7 @@
+const box = document.getElementById("theText")
+const newValue = function(){
+    const button = document.getElementById("submit").addEventListener("click", newValue)
+    if(box.value === "") {
+        alert(box.value)
+    }
+}
